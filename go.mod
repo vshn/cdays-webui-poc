4f00e3e7f28ae70ba923e1b5ded22d0fbd268bb2
Tobias Brunner <tobias@tobru.ch>
2019-05-01 17:40:21 +0200

    swagger api client

--- go.mod
+++ go.mod
@@ -7,6 +7,12 @@ require (
 	github.com/codegangsta/envy v0.0.0-20141216192214-4b78388c8ce4 // indirect
 	github.com/codegangsta/gin v0.0.0-20171026143024-cafe2ce98974 // indirect
 	github.com/foolin/goview v0.1.0
+	github.com/go-openapi/errors v0.19.0
+	github.com/go-openapi/runtime v0.19.0
+	github.com/go-openapi/spec v0.18.0
+	github.com/go-openapi/strfmt v0.18.0
+	github.com/go-openapi/swag v0.18.0
+	github.com/go-openapi/validate v0.18.0
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/markbates/refresh v1.7.1 // indirect
 	github.com/mattn/go-shellwords v1.0.5 // indirect
---
+++ test_api.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	httptransport "github.com/go-openapi/runtime/client"
+	"github.com/go-openapi/strfmt"
+	apiclient "github.com/vshn/cdays-webui-poc/client"
+	"github.com/vshn/cdays-webui-poc/client/operations"
+)
+
+func main() {
+
+	// create the transport
+	transport := httptransport.New("127.0.0.1:8080", "", nil)
+
+	// create the API client, with the transport
+	client := apiclient.New(transport, strfmt.Default)
+
+	// to override the host for the default client
+	// apiclient.Default.SetTransport(transport)
+
+	// make the request to get all items
+	//resp, err := client.Operations.All(operations.AllParams{})
+	resp, err := client.Operations.GetManagedNamespaces(operations.NewGetManagedNamespacesParams())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%#v\n", resp.Payload)
+}
