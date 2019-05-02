18a0a0e63182af5b5d5a2b6801e2cb8c24174453
Tobias Brunner <tobias@tobru.ch>
2019-05-02 10:36:39 +0200

    namespace creating stubs

--- cmd/webui/main.go
+++ cmd/webui/main.go
@@ -20,15 +20,18 @@ import (
 	apiclient "github.com/vshn/cdays-webui-poc/client"
 )
 
+func createNamespace(c echo.Context) error {
+	nsName := c.FormValue("nsName")
+	return c.Render(http.StatusCreated, "createns", echo.Map{
+		"title":   "APPUiO Management API - Created Namespace " + nsName,
+		"message": "Namespace " + nsName + " successfully created",
+	})
+}
+
 func main() {
 
 	// Setup WebAPI Client
 	client := apiclient.New(httptransport.New("127.0.0.1:8080", "", nil), strfmt.Default)
-
-	resp, err := client.Operations.GetManagedNamespaces(operations.NewGetManagedNamespacesParams())
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	// Initiate and configure Echo
 	e := echo.New()
@@ -42,19 +45,20 @@ func main() {
 
 	// Routes
 	e.GET("/", func(c echo.Context) error {
-		//render with master
 		return c.Render(http.StatusOK, "index", echo.Map{
 			"title": "APPUiO Management API",
 		})
 	})
 	e.GET("/clusters", func(c echo.Context) error {
-		//render with master
 		return c.Render(http.StatusOK, "clusters", echo.Map{
 			"title": "APPUiO Management API - Clusters",
 		})
 	})
 	e.GET("/namespaces", func(c echo.Context) error {
-		//render with master
+		resp, err := client.Operations.GetManagedNamespaces(operations.NewGetManagedNamespacesParams())
+		if err != nil {
+			log.Fatal(err)
+		}
 		return c.Render(http.StatusOK, "namespaces", echo.Map{
 			"title":      "APPUiO Management API - Namespaces",
 			"namespaces": resp.Payload,
@@ -63,6 +67,13 @@ func main() {
 			},
 		})
 	})
+	e.GET("/createns", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "createns", echo.Map{
+			"title":   "APPUiO Management API - Create Namespace",
+			"message": nil,
+		})
+	})
+	e.POST("/createns", createNamespace)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
