74938e95503744d1e2bdcce4f04d7088c74d2b53
Tobias Brunner <tobias@tobru.ch>
2019-05-02 16:50:22 +0200

    frontend js table fitlering

--- static/js/webui.js
+++ static/js/webui.js
@@ -4,4 +4,22 @@
     feather.replace()
   
   })()
-  
+
+
+  function filterTable(event) {
+    var filter = event.target.value.toUpperCase();
+    var rows = document.querySelector("#theTable tbody").rows;
+
+    for (var i = 0; i < rows.length; i++) {
+        var firstCol = rows[i].cells[1].textContent.toUpperCase();
+        var secondCol = rows[i].cells[2].textContent.toUpperCase();
+        var thirdCol = rows[i].cells[3].textContent.toUpperCase();
+        if (firstCol.indexOf(filter) > -1 || secondCol.indexOf(filter) > -1 || thirdCol.indexOf(filter) > -1) {
+          rows[i].style.display = "";
+        } else {
+          rows[i].style.display = "none";
+        }
+    }
+}
+
+document.querySelector('#searchbar').addEventListener('keyup', filterTable, false);
